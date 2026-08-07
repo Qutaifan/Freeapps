c724775415509e163467870089c0fa0a1d15bd63
Qutaifan <qutaifan@qutaifan.com>
2026-08-07 21:42:17 +0300

    Remove middle nav links (Pillars, Directory, AI Prompt Gen, FAQ) from Navbar as requested, keeping logo and search/submit actions

--- freeapps-components/src/components/Navbar.tsx
+++ freeapps-components/src/components/Navbar.tsx
@@ -30,13 +30,6 @@ export function Navbar({ onOpenSearch, onOpenSubmit }: NavbarProps) {
         </div>
       </a>
 
-      <ul className="nav-links-group">
-        <li><a href="#categories" className="nav-link">Pillars</a></li>
-        <li><a href="#featured" className="nav-link">Directory</a></li>
-        <li><a href="#prompt-generator" className="nav-link">AI Prompt Gen</a></li>
-        <li><a href="#faq" className="nav-link">FAQ</a></li>
-      </ul>
-
       <div className="nav-right-actions">
         <button className="search-trigger-btn" onClick={onOpenSearch}>
           <span>Search directory...</span>
